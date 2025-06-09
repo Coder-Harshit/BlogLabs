24c80cd09f03443d82560088ac4a00f9c1f30c59
Harshit Vijay <93333205+Coder-Harshit@users.noreply.github.com>
2025-06-09 21:54:20 +0530

    bugfix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-<<<<<<< HEAD
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
-export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
-=======
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
->>>>>>> grub
 });
